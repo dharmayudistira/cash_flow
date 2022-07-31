4f63840f51536472d8cf5856ab45f165431b815b
Dharma Yudistira <dharmayudistira2000@gmail.com>
2022-07-31 19:25:48 +0700

    fix: loading when logout

--- lib/app/modules/setting/views/setting_view.dart
+++ lib/app/modules/setting/views/setting_view.dart
@@ -11,150 +11,150 @@ class SettingView extends GetView<SettingController> {
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      body: SafeArea(
-        child: Obx(() {
-          if (controller.isUpdateLogin.value) {
-            return LoadingWidget(child: _buildBodySetting(context));
-          } else {
-            return _buildBodySetting(context);
-          }
-        }),
-      ),
+      body: Obx(() {
+        if (controller.isUpdateLogin.value) {
+          return LoadingWidget(child: _buildBodySetting(context));
+        } else {
+          return _buildBodySetting(context);
+        }
+      }),
     );
   }
 
   Widget _buildBodySetting(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Expanded(
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    InkWell(
-                      highlightColor: Colors.transparent,
-                      splashColor: Colors.transparent,
-                      onTap: () {
-                        Get.back();
-                      },
-                      child: const Icon(Icons.arrow_back_ios),
-                    ),
-                    const SizedBox(width: 16),
-                    Text(
-                      "Setting",
-                      style: Theme.of(context).textTheme.headline3,
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 32),
-                TextField(
-                  controller: controller.oldPasswordController,
-                  readOnly: true,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(16)),
-                    ),
-                    labelText: "Current Password",
-                    filled: true,
-                    labelStyle: TextStyle(
-                      fontFamily: 'Decker',
-                      fontSize: 14,
-                      fontWeight: FontWeight.bold,
-                    ),
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Expanded(
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      InkWell(
+                        highlightColor: Colors.transparent,
+                        splashColor: Colors.transparent,
+                        onTap: () {
+                          Get.back();
+                        },
+                        child: const Icon(Icons.arrow_back_ios),
+                      ),
+                      const SizedBox(width: 16),
+                      Text(
+                        "Setting",
+                        style: Theme.of(context).textTheme.headline3,
+                      ),
+                    ],
                   ),
-                ),
-                const SizedBox(height: 8),
-                TextField(
-                  controller: controller.newPasswordController,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(16)),
-                    ),
-                    labelText: "New Password",
-                    filled: true,
-                    labelStyle: TextStyle(
-                      fontFamily: 'Decker',
-                      fontSize: 14,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 16),
-                SizedBox(
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.greenAccent,
-                    ),
-                    onPressed: () async {
-                      await controller.updateUser();
-                    },
-                    child: const Text("Save Password"),
-                  ),
-                ),
-                SizedBox(
-                  width: double.infinity,
-                  child: OutlinedButton(
-                    onPressed: () {
-                      Get.back();
-                    },
-                    child: const Text("Back"),
-                  ),
-                ),
-                SizedBox(
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.redAccent,
-                    ),
-                    onPressed: () async {
-                      await controller.logout();
-                    },
-                    child: const Text(
-                      "Logout",
-                      style: TextStyle(
-                        color: Colors.white,
+                  const SizedBox(height: 32),
+                  TextField(
+                    controller: controller.oldPasswordController,
+                    readOnly: true,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(16)),
+                      ),
+                      labelText: "Current Password",
+                      filled: true,
+                      labelStyle: TextStyle(
+                        fontFamily: 'Decker',
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
                   ),
-                ),
-              ],
-            ),
-          ),
-          Obx(
-            (() {
-              return Column(
-                children: [
-                  const Text("Develop by : "),
-                  ListTile(
-                    leading: Card(
-                      child: ClipRRect(
-                        borderRadius:
-                            const BorderRadius.all(Radius.circular(8)),
-                        child: Image.asset(
-                          "assets/photo_dharma.jpg",
-                          fit: BoxFit.fill,
+                  const SizedBox(height: 8),
+                  TextField(
+                    controller: controller.newPasswordController,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(16)),
+                      ),
+                      labelText: "New Password",
+                      filled: true,
+                      labelStyle: TextStyle(
+                        fontFamily: 'Decker',
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.greenAccent,
+                      ),
+                      onPressed: () async {
+                        await controller.updateUser();
+                      },
+                      child: const Text("Save Password"),
+                    ),
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    child: OutlinedButton(
+                      onPressed: () {
+                        Get.back();
+                      },
+                      child: const Text("Back"),
+                    ),
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.redAccent,
+                      ),
+                      onPressed: () async {
+                        await controller.logout();
+                      },
+                      child: const Text(
+                        "Logout",
+                        style: TextStyle(
+                          color: Colors.white,
                         ),
                       ),
                     ),
-                    title: Text(
-                      controller.currentUser.value!.name,
-                      style: Theme.of(context).textTheme.headline3,
-                    ),
-                    subtitle: Text(
-                      "1841720028",
-                      style: Theme.of(context).textTheme.bodyText1,
-                    ),
                   ),
                 ],
-              );
-            }),
-          ),
-        ],
+              ),
+            ),
+            Obx(
+              (() {
+                return Column(
+                  children: [
+                    const Text("Develop by : "),
+                    ListTile(
+                      leading: Card(
+                        child: ClipRRect(
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(8)),
+                          child: Image.asset(
+                            "assets/photo_dharma.jpg",
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                      ),
+                      title: Text(
+                        controller.currentUser.value!.name,
+                        style: Theme.of(context).textTheme.headline3,
+                      ),
+                      subtitle: Text(
+                        "1841720028",
+                        style: Theme.of(context).textTheme.bodyText1,
+                      ),
+                    ),
+                  ],
+                );
+              }),
+            ),
+          ],
+        ),
       ),
     );
   }
